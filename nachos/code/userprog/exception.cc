e8f9549414c3bc3e3b5e4a50abb1ea29c681e31d
Avikalp Kumar Gupta <avikalpg@csews9.cse.iitk.ac.in>
2014-11-10 17:10:05 +0530

    initial hai abhi bhi

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -300,7 +300,12 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
-    } else {
+    }
+///////////////////////// STARTING CHANGES FOR ASSIGNMENT3 ///////////////////////
+
+//////////////////////////// DONE CHANGES IN ASSIGNMENT 3 /////////////////////////////////////
+
+ else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
     }
